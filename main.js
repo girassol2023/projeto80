2d0397fe25b44c064db9f1910c3fb8ab08fc882f
girassol2023 <123131164+girassol2023@users.noreply.github.com>
2023-03-05 19:53:12 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+Lista_nomes = []
+function lista() {
+    i++
+    var nomes1 = document.getElementById("nomes1").value;
+
+    lista_nomes.push("nomes1");
+    document.getElementById("resultado").innerHTML = lista_nomes;
+
+}
